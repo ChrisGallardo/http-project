6a21b7c516c2d0eb83ad1ca0c8a0a7d360c0e564
Chris Gallardo <chrisgallardo@live.com>
2020-11-01 17:22:16 -0800

    Initial commit

---
+++ http-project/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace http_project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
